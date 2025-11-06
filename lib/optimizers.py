7f506c764f6c773392c15a125985452d3bf631b7
kwan <kwanhee.lee@postech.ac.kr>
2025-11-06 20:20:15 +0900

    init z0 as 0

--- lib/optimizers.py
+++ lib/optimizers.py
@@ -216,7 +216,9 @@ def get_admm_optimizer(base_optimizer_cls):
                 init_importance = None
 
             # Initial split z and initial_split (as bool)
-            z0 = p.detach().clone()
+            z0 = torch.zeros_like(
+                p, memory_format=torch.preserve_format
+            )
             
             if self.init_lambda_from_inv_resid:
                 initial_residual = torch.norm(p.detach() - z0.detach())
